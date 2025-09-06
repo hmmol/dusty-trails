b03c160ac0156798ec2aa0ce0b7b7ed93c9b4e0f
doridian <templarknight1066@gmail.com>
2025-09-05 17:30:29 -0700

    Initial commit

---
+++ Scripts/player.gd
@@ -0,0 +1,90 @@
+extends CharacterBody2D
+
+# Player movement speed
+@export var speed = 50
+
+# Node reference
+@onready var animation_sprite = $AnimatedSprite2D
+
+# Direction and animation to be updated throughout the game state
+var new_direction = Vector2(0,1) # only move one space
+var animation
+var is_attacking = false
+
+# ---------------------------------- Movement & Animations ---------------------------------------
+func _physics_process(delta):
+	# Get player input
+	var direction: Vector2
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	# If input is digital, normalize it for diagonal movement
+	if abs(direction.x) == 1 and abs(direction.y) == 1:
+		direction = direction.normalized()
+	
+	# Sprinting
+	if Input.is_action_pressed("ui_sprint"):
+		speed = 100
+	elif Input.is_action_just_released("ui_sprint"):
+		speed = 50
+	
+	# Apply movement
+	var movement = speed * direction * delta
+	if is_attacking == false:
+		# Moves the player around, whilst enforcing collisions so that they come to a stop when colliding
+		# with other objects
+		move_and_collide(movement)
+		# Plays animations
+		player_animations(direction)
+	# If no input is pressed, idle
+	if !Input.is_anything_pressed():
+		if is_attacking == false:
+			animation = "idle_" + returned_direction(new_direction)
+
+func _input(event):
+	# Input event for attacking/shooting
+	if event.is_action_pressed("ui_attack"):
+		# attacking/shooting animation
+		is_attacking = true
+		var animation = "attack_" + returned_direction(new_direction)
+		animation_sprite.play(animation)
+
+# Animations
+func player_animations(direction: Vector2):
+	# Vector2.ZERO is shorthand for writing Vector2(0,0)
+	if direction != Vector2.ZERO:
+		# Update direction with new_direction
+		new_direction = direction
+		# Play walk animation because we are moving
+		animation = "walk_" + returned_direction(new_direction)
+		animation_sprite.play(animation)
+	else:
+		# Play idle animation because we are still
+		animation = "idle_" + returned_direction(new_direction)
+		animation_sprite.play(animation)
+		
+# Animation direction
+func returned_direction(direction: Vector2):
+	# Normalize the direction vector
+	var normalized_drection = direction.normalized()
+	var default_return = "side"
+	
+	if normalized_drection.y > 0:
+		return "down"
+	elif normalized_drection.y < 0:
+		return "up"
+	elif normalized_drection.x > 0:
+		# (right)
+		$AnimatedSprite2D.flip_h = false
+		return "side"
+	elif normalized_drection.x < 0:
+		# (left)
+		$AnimatedSprite2D.flip_h = true
+		return "side"
+	
+	# Default value is empty
+	return default_return
+
+
+func _on_animated_sprite_2d_animation_finished():
+	is_attacking = false
